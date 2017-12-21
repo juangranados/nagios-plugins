7065cb76928217e9db815a5ee382c1002895dd49
juangranados <juangranados@gmail.com>
2017-12-21 12:21:25 +0100

    Update check_services.ps1

--- check_services.ps1
+++ check_services.ps1
@@ -1,14 +1,14 @@
 $Services = Get-CimInstance win32_service -Filter "startmode = 'auto' AND state != 'running' AND exitcode != 0"  | select name, startname, exitcode
 $ServicesRunning = Get-CimInstance win32_service -Filter "state = 'running'"
 if ([string]::IsNullOrEmpty($Services)){
-    Write-Output "OK: All services running | ServicesRunning=$($ServicesRunning.Count);0;0;0;0"
-    Exit 0
+    Write-Output "OK: All services running | ServicesRunning=$($ServicesRunning.Count)1;1;;;"
+    $host.SetShouldExit(0)
 }
 else{
     $ServicesStopped=""
     ForEach ($Service in $Services){
         $ServicesStopped += "$($Service.Name),"
     }
-    Write-Output "CRITICAL: Service(s) stopped: $($ServicesStopped.TrimEnd(",")) | ServicesRunning=$($ServicesRunning.Count);0;0;0;0"
-    Exit 2;
+    Write-Output "CRITICAL: Service(s) stopped: $($ServicesStopped.TrimEnd(",")) | ServicesRunning=$($ServicesRunning.Count);1;1;;"
+    $host.SetShouldExit(2);
 }
