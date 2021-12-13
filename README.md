7b29f0e83febed500499e62ddc08d4f09d5b2a55
Juan Granados <juangranados@gmail.com>
2021-12-14 00:02:08 +0100

    Upload Veeam script

--- README.md
+++ README.md
@@ -2,12 +2,13 @@
 
 Several Nagios/Icinga Plugins written in PowerShell. All of them return Nagios permonmance data and exit code.
 
-* [Active Directory Replication](https://github.com/juangranados/nagios-plugins/blob/master/check_adreplication.ps1): Check AD Replication in a DC Server.
-* [Azure AD Connect](https://github.com/juangranados/nagios-plugins/blob/master/check_azureadconnectsync.ps1): Check Azure AD Connect status and last replication.
-* [Backup Exec Jobs](https://github.com/juangranados/nagios-plugins/blob/master/check_bejobs.ps1): Check Backup Exec (2012-2017) last scheduled job status.
-* [Disk Fragmentation](https://github.com/juangranados/nagios-plugins/blob/master/check_diskdefragstatus.ps1): Check disks fragmentation status and optionally defrag them if warning or critical.
-* [Internet Information Server](https://github.com/juangranados/nagios-plugins/blob/master/check_iis.ps1): Check Internet Information Server Websites and AppPools and try to start (filtered) stopped ones.
-* [Windows Services](https://github.com/juangranados/nagios-plugins/blob/master/check_services.ps1): Check all automatic services are running and try to start stopped ones.
-* [Windows Update](https://github.com/juangranados/nagios-plugins/blob/master/check_updates.ps1): Check if there are additional updates that have not been applied to a Microsoft Windows machine.
-* [Windows Server Backup](https://github.com/juangranados/nagios-plugins/blob/master/check_wsb.ps1): Check Windows Server Backup last backup.
-* [Windows drives fragmentation](https://github.com/juangranados/nagios-plugins/blob/master/check_diskdefragstatus.ps1): Check Windows drives fragmentation and optionally defrag them.
+- [Active Directory Replication](https://github.com/juangranados/nagios-plugins/blob/master/check_adreplication.ps1): Check AD Replication in a DC Server.
+- [Azure AD Connect](https://github.com/juangranados/nagios-plugins/blob/master/check_azureadconnectsync.ps1): Check Azure AD Connect status and last replication.
+- [Backup Exec Jobs](https://github.com/juangranados/nagios-plugins/blob/master/check_bejobs.ps1): Check Backup Exec (2012-2017) last scheduled job status.
+- [Disk Fragmentation](https://github.com/juangranados/nagios-plugins/blob/master/check_diskdefragstatus.ps1): Check disks fragmentation status and optionally defrag them if warning or critical.
+- [Internet Information Server](https://github.com/juangranados/nagios-plugins/blob/master/check_iis.ps1): Check Internet Information Server Websites and AppPools and try to start (filtered) stopped ones.
+- [Veeam Jobs](https://github.com/juangranados/nagios-plugins/blob/master/check_veeamjobs.ps1): Check Veeam last result of all jobs.
+- [Windows Services](https://github.com/juangranados/nagios-plugins/blob/master/check_services.ps1): Check all automatic services are running and try to start stopped ones.
+- [Windows Update](https://github.com/juangranados/nagios-plugins/blob/master/check_updates.ps1): Check if there are additional updates that have not been applied to a Microsoft Windows machine.
+- [Windows Server Backup](https://github.com/juangranados/nagios-plugins/blob/master/check_wsb.ps1): Check Windows Server Backup last backup.
+- [Windows drives fragmentation](https://github.com/juangranados/nagios-plugins/blob/master/check_diskdefragstatus.ps1): Check Windows drives fragmentation and optionally defrag them.
---
+++ check_veeamjobs.ps1
@@ -0,0 +1,102 @@
+<#
+.SYNOPSIS
+    Check Veeam last result and last run of all jobs.
+.DESCRIPTION
+    Check Veeam last result and last run of all jobs.
+.OUTPUTS
+    OK: All jobs last result is Success.
+    CRITICAL: Any job last result is warning or any job last run less than warning hours ago.
+    CRITICAL: Any job last result is error or any job last run less than critical hours ago.
+.PARAMETER warning
+    Warning hours after last backup.
+    Default 24.
+.PARAMETER critical
+    Warning hours after last backup.
+    Default 48.
+.PARAMETER jobs
+    Jobs names to check.
+    Default: all.
+    Example: "Tape Backup","Hyper-V","Incremental backup"
+.EXAMPLE
+    Checks jobs by name with default warning treshold and 50 critical treshold.
+    check_diskdefragstatus.ps1 -jobs "Tape Backup","Full Backup","Hyper-V Backup" -critical 50
+.EXAMPLE
+    Checks all jobs by name with 48 warning treshold and 96 critical treshold.
+    check_diskdefragstatus.ps1 -warning 48 -critical 96
+.NOTES 
+	Author:	Juan Granados
+#>
+Param(
+    [Parameter(Mandatory = $false, Position = 0)] 
+    [int]$warning = 24,
+    [Parameter(Mandatory = $false, Position = 1)] 
+    [int]$critical = 48,
+    [Parameter(Mandatory = $false, Position = 2)]
+    [string[]]$jobs = "all"
+)
+#Requires -RunAsAdministrator
+$ErrorActionPreference = "Stop"
+$global:nagiosStatus = 0
+$global:nagiosOutput = ""
+$WarningPreference = 'SilentlyContinue'
+Add-PSSnapin -Name VeeamPSSnapIn -ErrorAction SilentlyContinue
+function Get-JobStatus ([string]$name, [string]$result, [string]$state, [datetime]$lastRun) {
+    $jobInfo = "Name: $name - Result: $result - State: $state - Last run: $lastRun."
+    Write-Verbose $jobInfo
+    if (($result -ne 'Warning' -and $result -ne "Success") -or $lastRun -lt (Get-Date).AddHours(-$critical)) {
+        $global:nagiosOutput += "Critical -> $jobInfo"
+        $global:nagiosStatus = 2
+    }
+    elseif ($result -eq 'Warning' -or $lastRun -lt (Get-Date).AddHours(-$warning)) {
+        $global:nagiosOutput += "Warning -> $jobInfo"
+        $global:nagiosStatus = 1
+    }
+    else {
+        $global:nagiosOutput += "Ok: -> $jobInfo"
+    }   
+}
+try {
+    Write-Verbose "Getting jobs"
+    $computerJobs = Get-VBRJob
+    $tapeJobs = Get-VBRTapeJob
+}
+catch {
+    Write-Output "CRITICAL: $($_.Exception.Message)"
+    Exit(2)
+}
+if ($jobs -ne "all") {
+    foreach ($job in $jobs) {
+        if (!($computerJobs.Name -like $job) -and !($tapeJobs.Name -like $job)) {
+            Write-Output "CRITICAL: $job not detected"
+            Exit(2)
+        }
+    }
+}
+if ($computerJobs.Length -gt 0) {
+    foreach ($job in $computerJobs) {
+        if ($jobs -eq "all" -or $jobs -like $job.Name) {
+            Get-JobStatus $($job.Name) $($job.GetLastResult()) $($job.findlastsession().state) $($job.LatestRunLocal)
+        }
+    }
+}
+if ($tapeJobs.Length -gt 0) {
+    foreach ($job in $tapeJobs) {
+        if ($jobs -eq "all" -or $jobs -like $job.Name) {
+            Get-JobStatus $($job.Name) $($job.LastResult) $($job.LastState) $((Get-VBRSession -Job $TapeJobS[0] -Last).EndTime)
+        }
+    }
+}
+if ($global:nagiosStatus -eq 2) {
+    Write-Output "CRITICAL: $($global:nagiosOutput)"
+    Exit(2)
+}
+if ($global:nagiosStatus -eq 1) {
+    Write-Output "CRITICAL: $($global:nagiosOutput)"
+    Exit(1)
+}
+if ($global:nagiosOutput -eq "") {
+    Write-Output "UNKNOWN: No jobs found"
+    Exit(3)
+}
+Write-Output "OK: $($global:nagiosOutput)"
+Exit(0)
