ff12e3f67dcf187458e8ac02cd0da3aa05e1994e
Juan Granados <juangranados@gmail.com>
2022-03-16 23:54:29 +0100

    Add checks

--- check_wg_tunnels.sh
+++ check_wg_tunnels.sh
@@ -7,7 +7,7 @@
 # Usage: check_wg_tunnels.sh [options]
 # -h | --host: ip of device.
 # -t | --tunnels: list of tunnels to check. 
-#                 Syntax: "EndpointPairLocalAddr1-wgIpsecEndpointPairPeerAddr1 EndpointPairLocalAddr2-wgIpsecEndpointPairPeerAddr2"
+#                 Syntax "localIP1-PeerIP2 localIP1-PeerIP2 localIPn-PeerIPn\"
 #                 Example: "80.24.56.73-90.123.124.34 100.234.45.47-90.12.45.123 123.234.43.65-65.234.56.78"
 # -v | --version: snmp version. Default 2. Depends on version you must specify:
 #   2: -s | --string: snmp community string. Default public.
@@ -61,7 +61,7 @@ while [ $# -gt 0 ]; do
       echo "       2: -s | --string: snmp community string. Default public"
       echo "       3: -u | --user: user. -p | --pass: password"
       echo "   -t | --tunnels: list of tunnels to check"
-      echo "                   Syntax: \"EndpointPairLocalAddr1-wgIpsecEndpointPairPeerAddr1 EndpointPairLocalAddr2-wgIpsecEndpointPairPeerAddr2\""
+      echo "                   Syntax \"localIP1-PeerIP2 localIP1-PeerIP2 localIPn-PeerIPn\""
       echo "                   Example: \"80.24.56.73-90.123.124.34 100.234.45.47-90.12.45.123 123.234.43.65-65.234.56.78\""
       echo "Example: check_wg_tunnels.sh -h 192.168.2.100 -s publicsnmp -t \"80.24.56.73-90.123.124.34 100.234.45.47-90.12.45.123\""
       echo "Example: check_wg_tunnels.sh -h 192.168.2.100 -v 3 -u read -p 1234567789 -t \"80.24.56.73-90.123.124.34 100.234.45.47-90.12.45.123 123.234.43.65-65.234.56.78\""
@@ -74,6 +74,21 @@ while [ $# -gt 0 ]; do
   esac
   shift
 done
+
+# https://stackoverflow.com/a/13778973
+ipvalid() {
+  # Set up local variables
+  local ip=${1:-1.2.3.4}
+  local IFS=.; local -a a=($ip)
+  # Start with a regex format test
+  [[ $ip =~ ^[0-9]+(\.[0-9]+){3}$ ]] || return 1
+  # Test values of quads
+  local quad
+  for quad in {0..3}; do
+    [[ "${a[$quad]}" -gt 255 ]] && return 1
+  done
+  return 0
+}
 
 # Check arguments
 if ! [[ $(command -v snmpwalk) ]]
@@ -121,6 +136,26 @@ do
    up=0
    local=`echo "$t"  | cut -d "-" -f1`
    peer=`echo "$t"  | cut -d "-" -f2`
+   if ! [[ `echo "$t"  | grep -` ]]
+   then
+    echo "Unknown: bad 't' parameter. Syntax \"localIP1-PeerIP2 localIP1-PeerIP2 localIPn-PeerIPn\""
+    exit 3
+   fi
+   if [[ $local = $peer ]]
+   then
+    echo "Unknown: local IP and peer IP can not be the same. Syntax \"localIP1-PeerIP2 localIP1-PeerIP2 localIPn-PeerIPn\""
+    exit 3
+   fi
+   if ! ipvalid "$local"
+   then
+    echo "Unknown: $local is not a valid IP"
+    exit 3
+   fi
+   if ! ipvalid "$peer"
+   then
+    echo "Unknown: $peer is not a valid IP"
+    exit 3
+   fi
    arraylength=${#localAddr[@]}
    for (( i=0; i<${arraylength}; i++ ));
     do
